e708f3c76fa1b86bf73fd31cbabae07e376532df
Yves Van Broekhoven <yves@mrhenry.be>
2011-05-10 13:54:10 +0200

    Success callback function added, renamed plugin to ajaxform

--- jquery-ajaxform.js
+++ jquery-ajaxform.js
@@ -1,7 +1,7 @@
 /*
- * jQuery reFormer
+ * jQuery ajaxform
  *
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Yves Van Broekhoven
  */
 
@@ -10,8 +10,9 @@
   var _submit
   ,   _handleResponse
   ,   _handleError
+  ,   _successClb
   ;
-  
+
   _submit = function(event){
     var ctx = $(this);
     event.preventDefault();
@@ -21,22 +22,40 @@
       data:     ctx.serialize(),
       success:  function(data){
         _handleResponse(data, ctx);
+        _successClb(ctx);
       },
       error:    _handleError
     });
   };
-  
+
   _handleResponse = function(data, ctx){
     var form = $("#" + ctx[0].id, data).first();
     ctx.replaceWith(form);
   };
-  
+
   _handleError = function(){
     console.log("something went wrong baby");
   };
+
+  _successClb = function(ctx){
+    var opts = $.fn.ajaxform.options;
+    if ($.isFunction(opts.successClb)) {
+      opts.successClb.call();
+    }
+  };
+
+  $.fn.ajaxform = function(options){
+    var _this = $(this);
   
-  $.fn.reformer = function(){
-    return this.live("submit", _submit);
+    $.fn.ajaxform.options = options ? $.extend({}, $.fn.ajaxform.defaults, options) : $.fn.ajaxform.defaults;
+  
+    _this.live("submit", _submit);
+  
+    return _this;
+  };
+
+  $.fn.ajaxform.defaults = {
+    successClb: false
   };
   
 }(jQuery));
